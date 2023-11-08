a4cd5bf5448994ac60b721ac9c109da4ea73ab85
beiju <beiju@me.com>
2023-11-08 00:13:33 -0500

    Publicly expose Weather enum

--- fed/src/lib.rs
+++ fed/src/lib.rs
@@ -3,6 +3,7 @@ mod parse;
 mod fed_event;
 
 pub use parse::stream::{expansion_era_events, EXPANSION_ERA_START, EXPANSION_ERA_END};
+pub use eventually_api::Weather;
 pub use fed_event::*;
 pub use parse::{parse_feed_event, feed_event_from_json};
 pub use parse::error::FeedParseError;
