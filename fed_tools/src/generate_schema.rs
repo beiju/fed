df7abc6267a56ed7e24348cbc78df3bd6dc48693
beiju <beiju@me.com>
2026-01-01 22:01:52 -0800

    I'm scared

--- fed_tools/src/generate_schema.rs
+++ fed_tools/src/generate_schema.rs
@@ -2,70 +2,67 @@
 
 
 use itertools::Itertools;
+use schemars::generate::SchemaSettings;
+use schemars::Schema;
+use schemars::transform::Transform;
 use fed::FedEvent;
-use schemars::{
-    schema::SchemaObject,
-    visit::Visitor,
-    visit::visit_schema_object,
-};
-use schemars::r#gen::SchemaSettings;
-use schemars::schema::Schema::Object;
 
 #[derive(Debug, Clone)]
 pub struct SurfaceEnumTitle;
 
-impl Visitor for SurfaceEnumTitle {
-    fn visit_schema_object(&mut self, schema: &mut SchemaObject) {
+impl Transform for SurfaceEnumTitle {
+    fn transform(&mut self, schema: &mut Schema) {
         // this is good coding
-        if let Some(subschemas) = &mut schema.subschemas {
-            if let Some(object) = &mut subschemas.one_of {
-                for schema in object {
-                    if let Object(obj) = schema {
-                        let values = if let Some(values) = &mut obj.enum_values {
-                            values
-                        } else if let Some(properties) = &mut obj.object {
-                            if let Some(type_prop) = properties.properties.get_mut("type") {
-                                if let Object(obj) = type_prop {
-                                    if let Some(values) = &mut obj.enum_values {
-                                        values
-                                    } else {
-                                        continue;
-                                    }
-                                } else {
-                                    continue;
-                                }
-                            } else if let Some((name, _)) = properties.properties.iter().exactly_one().ok() {
-                                if let Some(metadata) = &mut obj.metadata {
-                                    metadata.title.get_or_insert(name.to_string());
-                                }
-                                continue;
-                            } else {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        };
-                        if let Some(first_value) = values.first() {
-                            if let Some(name) = first_value.as_str() {
-                                if let Some(metadata) = &mut obj.metadata {
-                                    metadata.title.get_or_insert(name.to_string());
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // Then delegate to default implementation to visit any subschemas
-        visit_schema_object(self, schema);
+        todo!("What is going on here")
+    //     if let Some(subschemas) = &mut schema {
+    //         if let Some(object) = &mut subschemas.one_of {
+    //             for schema in object {
+    //                 if let Object(obj) = schema {
+    //                     let values = if let Some(values) = &mut obj.enum_values {
+    //                         values
+    //                     } else if let Some(properties) = &mut obj.object {
+    //                         if let Some(type_prop) = properties.properties.get_mut("type") {
+    //                             if let Object(obj) = type_prop {
+    //                                 if let Some(values) = &mut obj.enum_values {
+    //                                     values
+    //                                 } else {
+    //                                     continue;
+    //                                 }
+    //                             } else {
+    //                                 continue;
+    //                             }
+    //                         } else if let Some((name, _)) = properties.properties.iter().exactly_one().ok() {
+    //                             if let Some(metadata) = &mut obj.metadata {
+    //                                 metadata.title.get_or_insert(name.to_string());
+    //                             }
+    //                             continue;
+    //                         } else {
+    //                             continue;
+    //                         }
+    //                     } else {
+    //                         continue;
+    //                     };
+    //                     if let Some(first_value) = values.first() {
+    //                         if let Some(name) = first_value.as_str() {
+    //                             if let Some(metadata) = &mut obj.metadata {
+    //                                 metadata.title.get_or_insert(name.to_string());
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     // Then delegate to default implementation to visit any subschemas
+    //     visit_schema_object(self, schema);
     }
 }
 
 
 fn main() {
     let schema = SchemaSettings::default()
-        .with_visitor(SurfaceEnumTitle)
+        .with_transform(SurfaceEnumTitle)
         .into_generator()
         .into_root_schema_for::<FedEvent>();
 
